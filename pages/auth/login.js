a9a88a28d480a1e0c421296efa7c08c12010b5dd
HARSH24-1997 <rajput24.harsh@gmail.com>
2021-05-09 17:58:44 +0530

    user

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,92 +1,110 @@
 import React from "react";
 import Link from "next/link";
-
-// layout for page
-
+import axios from 'axios'
 import Auth from "layouts/Auth.js";
+import {useRouter} from 'next/router';
+import {router} from "next/client";
+
+
+const onSubmit = async () => {
+
+    const email = document.getElementById('email').value,
+        password = document.getElementById('password').value;
+
+    const response = await axios.post('http://localhost:3000/api/login', {email, password});
+    if (response.status === 200) {
+        await router.push('/user/userNext');
+    } else {
+        console.log(response);
+    }
+}
 
 export default function Login() {
-  return (
-    <>
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-4/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-              <div className="flex-auto px-4 lg:px-10 py-10">
-                <form>
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-                      placeholder="Email"
-                    />
-                  </div>
+    return (
+        <>
+            <div className="container mx-auto px-4 h-full">
+                <div className="flex content-center items-center justify-center h-full">
+                    <div className="w-full lg:w-4/12 px-4">
+                        <div
+                            className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+                            <div className="flex-auto px-4 lg:px-10 py-10">
+                                <form>
+                                    <div className="relative w-full mb-3">
+                                        <label
+                                            className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                                            htmlFor="grid-password"
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            type="email"
+                                            className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+                                            placeholder="Email"
+                                            id='email'
+                                        />
+                                    </div>
 
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="grid-password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-                      placeholder="Password"
-                    />
-                  </div>
-                  <div>
-                    <label className="inline-flex items-center cursor-pointer">
-                      <input
-                        id="customCheckLogin"
-                        type="checkbox"
-                        className="form-checkbox text-gray-800 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                      />
-                      <span className="ml-2 text-sm font-semibold text-gray-700">
+                                    <div className="relative w-full mb-3">
+                                        <label
+                                            className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                                            htmlFor="grid-password"
+                                        >
+                                            Password
+                                        </label>
+                                        <input
+                                            type="password"
+                                            className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
+                                            placeholder="Password"
+                                            id='password'
+                                        />
+                                    </div>
+                                    <div>
+                                        <label className="inline-flex items-center cursor-pointer">
+                                            <input
+                                                id="customCheckLogin"
+                                                type="checkbox"
+                                                className="form-checkbox text-gray-800 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                                            />
+                                            <span className="ml-2 text-sm font-semibold text-gray-700">
                         Remember me
                       </span>
-                    </label>
-                  </div>
+                                        </label>
+                                    </div>
 
-                  <div className="text-center mt-6">
-                    <button
-                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                      Sign In
-                    </button>
-                  </div>
-                </form>
-              </div>
+                                    <div className="text-center mt-6">
+                                        <button
+                                            className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                                            type="button"
+                                            onClick={onSubmit}
+                                        >
+                                            Sign In
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="flex flex-wrap mt-6 relative">
+                            <div className="w-1/2">
+                                <a
+                                    onClick={(e) => e.preventDefault()}
+                                    className="text-gray-300"
+                                >
+                                    <small>Forgot password?</small>
+                                </a>
+                            </div>
+                            <div className="w-1/2 text-right">
+                                <Link href="/auth/register">
+                                    <a className="text-gray-300">
+                                        <small>Create new account</small>
+                                    </a>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-wrap mt-6 relative">
-              <div className="w-1/2">
-                <a
-                  onClick={(e) => e.preventDefault()}
-                  className="text-gray-300"
-                >
-                  <small>Forgot password?</small>
-                </a>
-              </div>
-              <div className="w-1/2 text-right">
-                <Link href="/auth/register">
-                  <a className="text-gray-300">
-                    <small>Create new account</small>
-                  </a>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 Login.layout = Auth;
